1eea021d6a2edbe592131d81910c73220832216b
gencive5 <vic.segen@gmail.com>
2025-01-28 00:35:07 +0100

    new pictures
    too many but let's see how it goes

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
 import creamsoda from "/assets/images/creamsoda2.jpg";
-import randy from "/assets/images/randy.jpg";
+import randi from "/assets/images/randi.jpg";
 import raisa2 from "/assets/images/raisa2.jpg";
 import sasha from "/assets/images/sasha.jpg";
+import sasha2 from "/assets/images/sasha2.jpg";
+import creamsoda3 from "/assets/images/creamsoda3.jpg";
+import fang from "/assets/images/fang.jpg";
+import fang2 from "/assets/images/fang2.jpg";
+import fang3 from "/assets/images/fang3.jpg";
+import fang357 from "/assets/images/fang357.jpg";
+import fang358 from "/assets/images/fang358.jpg";
+import roversi from "/assets/images/roversi.jpg";
+import knas from "/assets/images/knas.jpg";
+import knas3 from "/assets/images/knas3.jpg";
+import viktor5 from "/assets/images/viktor5.jpg";
+import viktor2 from "/assets/images/viktor2.jpg";
+import richemond from "/assets/images/richemond.jpg";
 
 const Portfolio = () => {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -38,18 +51,23 @@ const Portfolio = () => {
 
   const portfolioItems = [
     { src: creamsoda, title: "creamsoda" },
-    { src: randy, title: "randy" },
+    { src: randi, title: "randi" },
     { src: raisa2, title: "RaisaVanessa" },
     { src: sasha, title: "SashaGymn" },
-    { src: creamsoda, title: "creamsoda" },
-    { src: randy, title: "randy" },
-    { src: raisa2, title: "RaisaVanessa" },
-    { src: sasha, title: "SashaGymn" },
-    { src: sasha, title: "SashaGymn" },
-    { src: creamsoda, title: "creamsoda" },
-    { src: randy, title: "randy" },
-    { src: raisa2, title: "RaisaVanessa" },
-    { src: sasha, title: "SashaGymn" },
+    { src: creamsoda3, title: "creamsoda2" },
+    { src: sasha2, title: "SashaGymn2" },
+    { src: fang, title: "FangDong" },
+    { src: fang3, title: "FangDong" },
+    { src: fang2, title: "FangDong" },
+    { src: richemond, title: "Richemond" },
+    { src: fang357, title: "FangDong" },
+    { src: fang358, title: "FangDong" },
+    { src: roversi, title: "NicolaDelorme" },
+    { src: knas, title: "Knas" },
+    { src: knas3, title: "Knas" },
+    { src: viktor5, title: "ViktorVerana" },
+    { src: viktor2, title: "ViktorVerana" },
+    
   ];
 
   const handleImageClick = (image) => {
@@ -69,7 +87,7 @@ const Portfolio = () => {
           data-direction="vertical"
           className="scroll col-xs-12 col-sm-6 col-md-4"
         >
-          {portfolioItems.slice(0, 4).map((item, index) => (
+          {portfolioItems.slice(0, 7).map((item, index) => (
             <div className="portfolio-item" key={index}>
               <div className="hover-bg" onClick={() => handleImageClick(item)}>
                 <img
@@ -88,7 +106,7 @@ const Portfolio = () => {
           data-direction="vertical"
           className="scroll col-xs-12 col-sm-6 col-md-4"
         >
-          {portfolioItems.slice(4, 8).map((item, index) => (
+          {portfolioItems.slice(7, 12).map((item, index) => (
             <div className="portfolio-item" key={index}>
               <div className="hover-bg" onClick={() => handleImageClick(item)}>
                 <img
@@ -107,7 +125,7 @@ const Portfolio = () => {
           data-direction="vertical"
           className="scroll col-xs-12 col-sm-6 col-md-4"
         >
-          {portfolioItems.slice(8, 12).map((item, index) => (
+          {portfolioItems.slice(12, 17).map((item, index) => (
             <div className="portfolio-item" key={index}>
               <div className="hover-bg" onClick={() => handleImageClick(item)}>
                 <img
